77b15ef5dcd93c5ec60b67662c59e6402621dac7
Lucas Lima <lucaspecheche@gmail.com>
2020-03-12 05:43:08 -0300

    #Seeder - Times

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -9,7 +9,7 @@ class OrderSeeder extends Seeder
     public function run(): void
     {
         Customer::all()->each(static function(Customer $customer) {
-            $orders = factory(Order::class, random_int(1,50))->make();
+            $orders = factory(Order::class, random_int(1,20))->make();
 
             foreach ($orders as $order) {
                 $order->customer()->associate($customer);
